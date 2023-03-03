9e56e1bed62c58839612ed52d4d1b6730f2cc7c6
DucPhamH <ducphamhong2@gmail.com>
2023-03-03 13:28:17 +0700

    add componnents

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,22 @@
+import styles from './taskList.module.scss'
+interface TaskListProps {
+  doneTaskList?: boolean
+}
+export default function TaskList(props: TaskListProps) {
+  const { doneTaskList } = props
+  return (
+    <div className='mb-2'>
+      <h2 className={styles.title}>{doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}</h2>
+      <div className={styles.tasks}>
+        <div className={styles.task}>
+          <input type='checkbox' className={styles.taskCheckbox} />
+          <span className={styles.taskName}>Học bài</span>
+          <div className={styles.taskActions}>
+            <button className={styles.taskBtn}>✒</button>
+            <button className={styles.taskBtn}>🗑</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
